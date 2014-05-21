41657613312660a2b3d3811158ba5b041b3c5bdf
Ranjith Kumar <ranji2612@gmail.com>
2014-05-22 01:46:40 +0530

    Base Template
    Basic stuff with a MEAN Stack

---
+++ app/routes.js
@@ -0,0 +1,67 @@
+var MyTask = require('./models/todo');
+
+module.exports = function(app,db) {
+
+	// api ---------------------------------------------------------------------
+	// get all todos
+	
+	
+	// application -------------------------------------------------------------
+	app.get('/home', function(req, res) {
+		res.sendfile('./public/html/home.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
+	
+	
+	// get all tasks
+	app.get('/api/tasks', function(req, res) {
+
+		// use mongoose to get all tasks in the database
+		MyTask.find(function(err, tasks) {
+
+			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+			if (err)
+				res.send(err)
+
+			res.json(tasks); // return all tasks in JSON format
+		});
+	});
+	
+	// create todo and send back all tasks after creation
+	app.post('/api/tasks', function(req, res) {
+		console.log("working");
+		// create a todo, information comes from Angular
+		MyTask.create({
+			title: req.body.taskname,
+			description: req.body.taskdesc,
+			pc : 0,
+			assignee: "Ranjith"
+		},function(err, todo) {
+			if (err)
+				res.send(err);
+			MyTask.find(function(err,tasks) {
+				if (err)
+					res.send(err)
+				res.json(tasks);
+			
+				});
+		});
+		res.redirect('/home');
+	});
+	
+		// delete a task
+	app.delete('/api/tasks/:task_id', function(req, res) {
+		MyTask.remove({
+			_id : req.params.task_id
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+			
+			MyTask.find(function(err,tasks) {
+				if (err)
+					res.send(err)
+				res.json(tasks);
+				});
+		});
+	});
+	
+};
---
+++ public/core.js
@@ -0,0 +1,38 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	// when landing on the page, get all tasks and show them
+	$http.get('/api/tasks')
+		.success(function(data) {
+			$scope.tasks = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+	// when submitting the add form, send the text to the node API
+	$scope.createTask = function() {
+		console.log("In core");
+		$http.post('/api/tasks', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {}; // clear the form
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	// delete a task after checking it
+	$scope.deleteTask = function(id) {
+		$http.delete('/api/tasks/' + id)
+			.success(function(data) {
+				$scope.tasks = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+}
