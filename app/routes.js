c1096d35409db2c0c11ac701b5adeacb9ec668b4
Ranjith Kumar <ranji2612@gmail.com>
2014-06-16 22:59:35 +0530

    Task Filtering
    User will be able to see only the tasks created by him

--- app/routes.js
+++ app/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app, passport) {
 	app.get('/api/tasks', function(req, res) {
 	
 		// use mongoose to get all tasks in the database
-		MyTask.find(function(err, tasks) {
+		MyTask.find({ assignee:req.user.email},function(err, tasks) {
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
@@ -53,17 +53,17 @@ module.exports = function(app, passport) {
 	
 	// create tasks and send back all tasks after creation
 	app.post('/api/tasks', function(req, res) {
-	
+		console.log("new log msg");console.log(req.user.email);
 		// create a todo, information comes from Angular
 		MyTask.create({
 			title: req.body.taskname,
 			description: req.body.taskdesc,
 			pc : 0,
-			assignee: "Ranjith"
+			assignee: req.user.email
 		},function(err, todo) {
 			if (err)
 				res.send(err);
-			MyTask.find(function(err,tasks) {
+			MyTask.find({ assignee:req.user.email},function(err,tasks) {
 				if (err)
 					res.send(err)
 				res.json(tasks);
@@ -79,7 +79,7 @@ module.exports = function(app, passport) {
 			if (err)
 				res.send(err);
 			
-			MyTask.find(function(err,tasks) {
+			MyTask.find({ assignee:req.user.email},function(err,tasks) {
 				if (err)
 					res.send(err)
 				res.json(tasks);
