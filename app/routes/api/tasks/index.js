eafdbe86fb846a835c512b61dab0b7e1706923c9
Ranjith Kumar <ranji2612@gmail.com>
2015-02-27 17:51:14 +0530

    Basic task paramerters REST calls

--- app/routes/api/tasks/index.js
+++ app/routes/api/tasks/index.js
@@ -8,17 +8,24 @@ var express = require('express');
 var router = express.Router();
 
 
-//Getting the current rate
-router.post('/', function( req, res) {
-	// Need to add Threshold also
-	//If Date is empty, set for next 1 or 2 or 3 months
-	
+//Get all the task
+router.get('/', function( req, res) {
+	//Return first 10 or 20 tasks based on some criteria
+	taskModel.find({}, function(err, data) {
+		if (err) res.send(400);
+		res.json(data);
+	});
 
 });
 
-//For Graph - Getting the past record of cheapest rate on that date
-router.post('/rateChange', function(req,res) {
-		
+//Create a new task
+router.post('/', function(req,res) {
+	console.log(req.body.cd);
+	console.log(req.body.ed);
+	taskModel.create({tn: req.body.tn, cb: req.body.cb, cd: req.body.cd, ed: req.body.ed, nos: req.body.nos, st : req.body.st},function(err, data) {
+		if (err) res.send(400);
+		res.json(200);
+	});
 });
 
 module.exports = router;
--- app/routes/api/tasks/tasks.model.js
+++ app/routes/api/tasks/tasks.model.js
@@ -3,6 +3,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var dbSchema = new Schema({
+	tn	: String,
+	cb	: String,
+	cd	: Number,
+	ed	: Number,
+	nos	: Number,
+	st	: Boolean
   	},{ collection: 'tasksCollection' });
 
 module.exports = mongoose.model('tasksModel', dbSchema);
